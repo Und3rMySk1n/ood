d23e3b1c97763062b543dc40474285e8793b363d
und3rmysk1n <undermyskin@inbox.ru>
2017-06-01 19:24:31 +0300

    Добавил интерфейс.

--- lab04_factory/Painter/Painter.cpp
+++ lab04_factory/Painter/Painter.cpp
@@ -2,13 +2,32 @@
 //
 
 #include "stdafx.h"
+#include "../libpainter/Canvas.h"
+#include "../libpainter/Painter.h"
 #include "../libpainter/Designer.h"
+#include "../libpainter/ShapeFactory.h"
+#include "../libpainter/PictureDraft.h"
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-	//CDesigner designer;
+	CCanvas canvas{ cout };
+	CPainter painter;
+	CPictureDraft draft;
+	CShapeFactory shapeFactory;
+	CDesigner designer(shapeFactory);
+
+	try
+	{
+		draft = designer.CreateDraft(cin);
+		painter.DrawPicture(draft, canvas);
+	}
+	catch (exception &e)
+	{
+		cout << e.what() << endl;
+	}	
 
 	return 0;
 }
