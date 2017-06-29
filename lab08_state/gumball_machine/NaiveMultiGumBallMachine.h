96d8e46e6e5ff083cca1cabdb9a3be46556c95a8
und3rmysk1n <undermyskin@inbox.ru>
2017-06-29 16:12:21 +0300

    добавил возможность добавлять несколько монет в наивно реализации и реализации с паттерном "состояние". Добавил тесты.

---
+++ lab08_state/gumball_machine/NaiveMultiGumBallMachine.h
@@ -0,0 +1,164 @@
+#pragma once
+
+#include <iostream>
+#include <boost/format.hpp>
+
+namespace naive
+{
+	class CMultiGumballMachine
+	{
+	public:
+		enum class State
+		{
+			SoldOut,		// Жвачка закончилась
+			NoQuarter,		// Нет монетки
+			HasQuarters,		// Есть монеты
+			Sold,			// Монетка выдана
+		};
+
+		CMultiGumballMachine(unsigned count)
+			: m_count(count)
+			, m_state(count > 0 ? State::NoQuarter : State::SoldOut)
+		{
+		}
+
+		void InsertQuarter()
+		{
+			using namespace std;
+			switch (m_state)
+			{
+			case State::SoldOut:
+				cout << "You can't insert a quarter, the machine is sold out\n";
+				break;
+			case State::NoQuarter:
+				cout << "You inserted a quarter\n";
+				m_quarters = 1;
+				m_state = State::HasQuarters;
+				break;
+			case State::HasQuarters:
+				if (m_quarters < 5)
+				{
+					++m_quarters;
+					cout << "You inserted a quarter\n";
+				}
+				else
+				{
+					cout << "You can't insert more quarters\n";
+				}
+				break;
+			case State::Sold:
+				cout << "Please wait, we're already giving you a gumball\n";
+				break;
+			}
+		}
+
+		void EjectQuarter()
+		{
+			using namespace std;
+			switch (m_state)
+			{
+			case State::HasQuarters:
+				cout << "Quarters returned\n";
+				m_quarters = 0;
+				m_state = State::NoQuarter;
+				break;
+			case State::NoQuarter:
+				cout << "You haven't inserted a quarter\n";
+				break;
+			case State::Sold:
+				cout << "Sorry you already turned the crank\n";
+				break;
+			case State::SoldOut:
+				if ((m_quarters) != 0)
+				{
+					m_quarters = 0;
+					cout << "Quarters returned\n";
+				}
+				else
+				{
+					cout << "You can't eject, you haven't inserted quarters yet\n";
+				}
+				break;
+			}
+		}
+
+		void TurnCrank()
+		{
+			using namespace std;
+			switch (m_state)
+			{
+			case State::SoldOut:
+				cout << "You turned but there's no gumballs\n";
+				break;
+			case State::NoQuarter:
+				cout << "You turned but there's no quarter\n";
+				break;
+			case State::HasQuarters:
+				cout << "You turned...\n";
+				m_state = State::Sold;
+				Dispense();
+				break;
+			case State::Sold:
+				cout << "Turning twice doesn't get you another gumball\n";
+				break;
+			}
+		}
+
+		void Refill(unsigned numBalls)
+		{
+			m_count = numBalls;
+			m_state = numBalls > 0 ? State::NoQuarter : State::SoldOut;
+		}
+
+		std::string ToString()const
+		{
+			std::string state =
+				(m_state == State::SoldOut) ? "sold out" :
+				(m_state == State::NoQuarter) ? "waiting for quarter" :
+				(m_state == State::HasQuarters) ? "waiting for turn of crank"
+				: "delivering a gumball";
+			auto fmt = boost::format(R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: %1% gumball%2%
+Quarters: %3%
+Machine is %4%
+)");
+			return (fmt % m_count % (m_count != 1 ? "s" : "") % m_quarters % state).str();
+		}
+
+	private:
+		void Dispense()
+		{
+			using namespace std;
+			switch (m_state)
+			{
+			case State::Sold:
+				cout << "A gumball comes rolling out the slot\n";
+				--m_quarters;
+				--m_count;
+				if (m_count == 0)
+				{
+					cout << "Oops, out of gumballs\n";
+					m_state = State::SoldOut;
+				}
+				else
+				{
+					m_state = State::NoQuarter;
+				}
+				break;
+			case State::NoQuarter:
+				cout << "You need to pay first\n";
+				break;
+			case State::SoldOut:
+			case State::HasQuarters:
+				cout << "No gumball dispensed\n";
+				break;
+			}
+		}
+
+		unsigned m_count;	 // Количество шариков
+		int m_quarters = 0;  // Количество монет
+		State m_state = State::SoldOut;
+	};
+}
---
+++ lab08_state/gumball_machine_tests/NaiveMultiGumBallTests.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include <boost/format.hpp>
+#include "../gumball_machine/NaiveMultiGumBallMachine.h"
+
+using namespace std;
+
+struct Multi_gumball_machine_
+{
+	stringstream outputStream;
+	naive::CMultiGumballMachine machine{ 3 };
+};
+
+BOOST_FIXTURE_TEST_SUITE(Multi_gumball_machine, Multi_gumball_machine_)
+	BOOST_AUTO_TEST_SUITE(when_created)
+		BOOST_AUTO_TEST_CASE(has_right_number_of_gumballs_and_quarters_and_state)
+		{
+			string expectedOutput = R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: 3 gumballs
+Quarters: 0
+Machine is waiting for quarter
+)";
+			outputStream << machine.ToString();
+			BOOST_CHECK_EQUAL(expectedOutput, outputStream.str());
+		}
+
+		BOOST_AUTO_TEST_CASE(increases_number_of_quarters_ang_change_state_if_we_insert_coin)
+		{
+			string expectedOutput = R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: 3 gumballs
+Quarters: 1
+Machine is waiting for turn of crank
+)";
+			machine.InsertQuarter();
+			outputStream << machine.ToString();
+			BOOST_CHECK_EQUAL(expectedOutput, outputStream.str());
+		}
+
+		BOOST_AUTO_TEST_CASE(can_not_carry_more_than_5_coins)
+		{
+			string expectedOutput = R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: 3 gumballs
+Quarters: 5
+Machine is waiting for turn of crank
+)";
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+
+			outputStream << machine.ToString();
+			BOOST_CHECK_EQUAL(expectedOutput, outputStream.str());
+		}
+
+		BOOST_AUTO_TEST_CASE(returns_all_quarters)
+		{
+			string expectedOutput = R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: 3 gumballs
+Quarters: 0
+Machine is waiting for quarter
+)";
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.EjectQuarter();
+
+			outputStream << machine.ToString();
+			BOOST_CHECK_EQUAL(expectedOutput, outputStream.str());
+		}
+
+		BOOST_AUTO_TEST_CASE(returns_all_quarters_even_if_machine_is_in_sold_out_state)
+		{
+			string expectedOutput = R"(
+Mighty Gumball, Inc.
+C++-enabled Standing Gumball Model #2016
+Inventory: 0 gumballs
+Quarters: 0
+Machine is sold out
+)";
+			machine.InsertQuarter();
+			machine.TurnCrank();
+			machine.InsertQuarter();
+			machine.TurnCrank();
+			machine.InsertQuarter();
+			machine.TurnCrank();
+			machine.InsertQuarter();
+			machine.InsertQuarter();
+			machine.EjectQuarter();
+
+			outputStream << machine.ToString();
+			BOOST_CHECK_EQUAL(expectedOutput, outputStream.str());
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
