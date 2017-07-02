55967399771ebfa79b93f10c09e4218e27ef0f66
und3rmysk1n <undermyskin@inbox.ru>
2017-07-02 16:07:29 +0300

    Переопределил яисто виртуальные функции для класса CGroupShape.

--- lab07_composite/composite/main.cpp
+++ lab07_composite/composite/main.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <limits>
 #include <functional>
+#include <vector>
 
 using boost::optional;
 
@@ -30,6 +31,33 @@ public:
 	virtual ~IStyle() = default;
 };
 
+class CStyle : public IStyle
+{
+public:
+	virtual optional<bool> IsEnabled()const override
+	{
+		return m_isEnabled;
+	}
+
+	virtual void Enable(bool enable) override
+	{
+		m_isEnabled = enable;
+	}
+
+	virtual optional<RGBAColor> GetColor()const override
+	{
+		return m_color;
+	}
+
+	virtual void SetColor(RGBAColor color) override
+	{
+		m_color = color;
+	}
+private:
+	bool m_isEnabled = false;
+	RGBAColor m_color = NULL;
+};
+
 class IGroupShape;
 
 class IShape : public IDrawable
@@ -38,11 +66,11 @@ public:
 	virtual RectD GetFrame() = 0;
 	virtual void SetFrame(const RectD & rect) = 0;
 
-	virtual IStyle & GetOutlineStyle() = 0;
-	virtual const IStyle & GetOutlineStyle()const = 0;
+	virtual std::shared_ptr<IStyle> GetOutlineStyle() = 0;
+	virtual const std::shared_ptr<IStyle> GetOutlineStyle()const = 0;
 	
-	virtual IStyle & GetFillStyle() = 0;
-	virtual const IStyle & GetFillStyle()const = 0;
+	virtual std::shared_ptr<IStyle> GetFillStyle() = 0;
+	virtual const std::shared_ptr<IStyle> GetFillStyle()const = 0;
 
 	virtual std::shared_ptr<IGroupShape> GetGroup() = 0;
 	virtual std::shared_ptr<const IGroupShape> GetGroup() const = 0;
@@ -78,10 +106,87 @@ public:
 	}
 };
 
+
+
 class CGroupShape : public IGroupShape
 {
+	CGroupShape()
+	{
+	}
 
+	virtual void Draw(const ICanvas & canvas) override
+	{
+		for (auto shape : m_shapes)
+		{
+			shape->Draw(canvas);
+		}
+	}
+
+	virtual std::shared_ptr<IStyle> GetOutlineStyle() override
+	{
+		return nullptr;
+	}
+
+	virtual const std::shared_ptr<IStyle> GetOutlineStyle()const override
+	{
+		return nullptr;
+	}
+
+	virtual std::shared_ptr<IStyle> GetFillStyle() override
+	{
+		return nullptr;
+	}
+
+	virtual const std::shared_ptr<IStyle> GetFillStyle()const override
+	{
+		return nullptr;
+	}
+
+	virtual std::shared_ptr<IGroupShape> GetGroup() override
+	{
+		return std::make_shared<IGroupShape>(this);
+	}
+
+	virtual std::shared_ptr<const IGroupShape> GetGroup() const override
+	{
+		return std::make_shared<IGroupShape>(this);
+	}
+
+	virtual void InsertShape(const std::shared_ptr<IShape> & shape, size_t position = std::numeric_limits<size_t>::max()) override
+	{
+		m_shapes.push_back(shape);
+	}
+
+	virtual std::shared_ptr<IShape> GetShapeAtIndex(size_t index) override
+	{
+		return m_shapes.at(index);
+	}
+
+	virtual void RemoveShapeAtIndex(size_t index) override
+	{
+		m_shapes.erase(m_shapes.begin() + index);
+	}
+
+	virtual size_t GetShapesCount()const override
+	{
+		return m_shapes.size();
+	}
+
+	virtual RectD GetFrame() override
+	{
+		return m_frame;
+	}
+
+	virtual void SetFrame(const RectD & rect) override
+	{
+		m_frame = rect;
+	}
+private:
+	RectD m_frame;
+	std::vector<std::shared_ptr<IShape>> m_shapes;
 };
+
+
 
 class ISlide : public IDrawable
 {
@@ -103,5 +208,4 @@ public:
 
 int main()
 {
-
 }
