91572c8f3c611fff8c2b30cfd39e2c8db6fc3126
und3rmysk1n <undermyskin@inbox.ru>
2017-07-06 18:03:17 +0300

    Добавил тесты для цветных фигур.

--- lab06_adapter/lwtask-tests/AdapterTests.cpp
+++ lab06_adapter/lwtask-tests/AdapterTests.cpp
@@ -62,8 +62,13 @@ struct Painter_
 {
 	stringstream outputStream;
 	graphics_lib::CCanvas canvas{ outputStream };
+
 	shape_drawing_lib::CRectangle rectangle{ { 0, 0 }, 5, 10 };
 	shape_drawing_lib::CTriangle triangle{ { 0, 0 },{ 1, 1 },{ 2, 2 } };
+	shape_drawing_lib::CRectangle greenRectangle{ { 0, 0 }, 5, 10, 65280 }; // #00ff00
+	shape_drawing_lib::CTriangle orangeTriangle{ { 0, 0 },{ 1, 1 },{ 2, 2 }, 16748544 }; // #ff9000
+
+
 	shape_drawing_lib::CCanvasPainter painter{ canvas };
 };
 
@@ -95,6 +100,33 @@ LineTo (0, 0)
 )";
 			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
 		}
+
+		BOOST_AUTO_TEST_CASE(can_draw_colored_rectangles)
+		{
+			painter.Draw(greenRectangle);
+			auto expectedCanvasContent =
+				R"(SetColor #00ff00
+MoveTo (0, 0)
+LineTo (5, 0)
+LineTo (5, 10)
+LineTo (0, 10)
+LineTo (0, 0)
+)";
+			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_draw_colored_triangles)
+		{
+			painter.Draw(orangeTriangle);
+			auto expectedCanvasContent =
+				R"(SetColor #ff9000
+MoveTo (0, 0)
+LineTo (1, 1)
+LineTo (2, 2)
+LineTo (0, 0)
+)";
+			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
+		}
 	BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 
@@ -107,6 +139,10 @@ struct CanvasAdapter_
 
 	shape_drawing_lib::CRectangle rectangle{ { 0, 0 }, 5, 10 };
 	shape_drawing_lib::CTriangle triangle{ { 0, 0 },{ 1, 1 },{ 2, 2 } };
+	shape_drawing_lib::CRectangle greenRectangle{ { 0, 0 }, 5, 10, 65280 }; // #00ff00
+	shape_drawing_lib::CTriangle orangeTriangle{ { 0, 0 },{ 1, 1 },{ 2, 2 }, 16748544 }; // #ff9000
+
+
 	shape_drawing_lib::CCanvasPainter painter{ canvasAdapter };
 };
 
@@ -135,7 +171,7 @@ BOOST_FIXTURE_TEST_SUITE(CanvasAdapter, CanvasAdapter_)
 </draw>
 )";
 			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
-		}		
+		}
 
 		BOOST_AUTO_TEST_CASE(can_draw_triangles_on_renderer)
 		{
@@ -153,6 +189,53 @@ BOOST_FIXTURE_TEST_SUITE(CanvasAdapter, CanvasAdapter_)
   </line>
   <line fromX="2" fromY="2" toX="0" toY="0">
     <color r="0.0" g="0.0" b="0.0" a="1.0" />
+  </line>
+</draw>
+)";
+			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_draw_colored_rectangles_on_renderer)
+		{
+			renderer.BeginDraw();
+			painter.Draw(greenRectangle);
+			renderer.EndDraw();
+
+			auto expectedCanvasContent =
+				R"(<draw>
+  <line fromX="0" fromY="0" toX="5" toY="0">
+    <color r="0.0" g="1.0" b="0.0" a="1.0" />
+  </line>
+  <line fromX="5" fromY="0" toX="5" toY="10">
+    <color r="0.0" g="1.0" b="0.0" a="1.0" />
+  </line>
+  <line fromX="5" fromY="10" toX="0" toY="10">
+    <color r="0.0" g="1.0" b="0.0" a="1.0" />
+  </line>
+  <line fromX="0" fromY="10" toX="0" toY="0">
+    <color r="0.0" g="1.0" b="0.0" a="1.0" />
+  </line>
+</draw>
+)";
+			BOOST_CHECK_EQUAL(outputStream.str(), expectedCanvasContent);
+		}
+
+		BOOST_AUTO_TEST_CASE(can_draw_colored_triangles_on_renderer)
+		{
+			renderer.BeginDraw();
+			painter.Draw(orangeTriangle);
+			renderer.EndDraw();
+
+			auto expectedCanvasContent =
+				R"(<draw>
+  <line fromX="0" fromY="0" toX="1" toY="1">
+    <color r="1.0" g="0.6" b="0.0" a="1.0" />
+  </line>
+  <line fromX="1" fromY="1" toX="2" toY="2">
+    <color r="1.0" g="0.6" b="0.0" a="1.0" />
+  </line>
+  <line fromX="2" fromY="2" toX="0" toY="0">
+    <color r="1.0" g="0.6" b="0.0" a="1.0" />
   </line>
 </draw>
 )";
